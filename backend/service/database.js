1c09f89d0d1efd211366d8b1e9563b1219619f63
Taming31 <sirasak.su@ku.th>
2025-12-09 22:51:24 +0700

    update

--- backend/service/database.js
+++ backend/service/database.js
@@ -2,16 +2,18 @@ import pkg from "pg"
 import dotenv from "dotenv"
 
 dotenv.config()
-const {Pool} = pkg
 
+const { Pool } = pkg
 
-const DBSERVER = process.env.DBSERVER
-const DBUSER = process.env.DBUSER
-const DBPWD = process.env.DBPWD
-const DBHOST = process.env.DBHOST
-const DBPORT = process.env.DBPORT
-const DB = process.env.DB
-
-export default new Pool({
-    connectionString:`${DBSERVER}://${DBUSER}:${encodeURIComponent(DBPWD)}@${DBHOST}:${DBPORT}/${DB}`
+const pool = new Pool({
+  user: process.env.DBUSER,
+  host: process.env.DBHOST,
+  database: process.env.DB,
+  password: process.env.DBPWD,
+  port: process.env.DBPORT,
+  ssl: {
+    rejectUnauthorized: false  
+  }
 })
+
+export default pool
